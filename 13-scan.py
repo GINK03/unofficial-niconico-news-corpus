78a472d66a25fee0b43467ea209afb0887f9db24
gimpei <gim.kobayashi@gmail.com>
2017-12-30 17:10:26 +0900

    a

---
+++ 13-scan.py
@@ -0,0 +1,39 @@
+import requests
+
+import dbm
+
+import time
+
+import concurrent.futures
+
+import random
+base_url = 'http://news.nicovideo.jp/watch/nw{}'
+
+headers = {'User-agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}
+
+def _map(arr):
+  index, iss = arr
+  db = dbm.open('dbms/htmls_{:09d}.dbm'.format(index), 'c')
+  for i in iss:
+    url = base_url.format(i)
+    if db.get(url) is not None:
+      continue
+    r = requests.get(base_url.format(i), headers = headers)
+    r.encoding = r.apparent_encoding 
+    if random.random() < 0.01:
+      print(r.text)
+    db[url] = r.text
+  db.close()
+  time.sleep(1)
+
+arrs = {}
+for index, i in enumerate(sorted(range(0, 3175136), key=lambda x:x*-1)):
+  key = index%32
+  if arrs.get(key) is None:
+    arrs[key] = []
+  arrs[key].append( i )
+arrs = [ (index, iss) for index, iss in arrs.items() ] 
+print("start to scan")
+#_map(arrs[-1])
+with concurrent.futures.ProcessPoolExecutor(max_workers=32) as ex:
+  ex.map(_map, arrs) 
---
+++ 16-time-series.py
@@ -0,0 +1,22 @@
+import dbm
+
+import pickle
+
+import datetime
+db = dbm.open('14-wakati.dbm', 'r')
+
+key_values = {}
+for key in db.keys():
+  obj = pickle.loads( db[key] )
+  try:
+    dtime = datetime.datetime.strptime(obj['time'], "%Y年%m月%d日 %H時%M分") # 2017年12月26日 15時51分
+  except ValueError as ex:
+    dtime = datetime.datetime.strptime(obj['time'], "%Y/%m/%d %H:%M") # 2017年12月26日 15時51分
+
+  key = dtime.strftime("%Y-%m-%d")
+  if key_values.get(key) is None:
+    key_values[key] = []
+  key_values[key].append( obj )
+
+for key, values in sorted( key_values.items(), key=lambda x:x[0]):
+  print(key, len(values))
