92a4b98e5a1f74e503a7d7d84df8ac36de8415ee
nardtree <gim.kobayashi@gmail.com>
2018-11-13 00:06:17 +0900

    update

--- 13-scan.py
+++ 13-scan.py
@@ -5,7 +5,7 @@ import concurrent.futures
 import random
 from pathlib import Path
 from hashlib import sha256
-
+import gzip
 base_url = 'http://news.nicovideo.jp/watch/nw{}'
 headers = {'User-agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}
 
@@ -21,12 +21,12 @@ def _map(arr):
       r.encoding = r.apparent_encoding 
       if random.random() < 0.01:
         print(r.text)
-      Path('htmls/' + hashed).open('w').write( r.text )
+      Path('htmls/' + hashed).open('wb').write( gzip.compress(bytes(r.text,'utf8')) )
     except Exception as ex:
       print(ex)
 
 arrs = {}
-for index, i in enumerate(sorted(range(3_500_000, 4_170_719), key=lambda x:x*-1)):
+for index, i in enumerate(sorted(range(1_500_000, 4_170_719), key=lambda x:x*-1)):
   key = index%32
   if arrs.get(key) is None:
     arrs[key] = []
@@ -34,5 +34,5 @@ for index, i in enumerate(sorted(range(3_500_000, 4_170_719), key=lambda x:x*-1)
 arrs = [ (index, iss) for index, iss in arrs.items() ] 
 print("start to scan")
 #_map(arrs[-1])
-with concurrent.futures.ProcessPoolExecutor(max_workers=64) as ex:
+with concurrent.futures.ProcessPoolExecutor(max_workers=32) as ex:
   ex.map(_map, arrs) 
